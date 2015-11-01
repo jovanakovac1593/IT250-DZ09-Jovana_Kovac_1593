1b50286f36e5baf9a019ef53db61332009af6bd3
vukasinovic <vukasinovic@outlook.com>
2015-11-01 18:54:05 +0100

    google maps - oglasa

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -2,7 +2,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngStorage', 'ngResource', 'ngCordova', 'jett.ionic.filter.bar'])
+angular.module('starter', ['ionic', 'ngStorage', 'ngResource', 'ngCordova', 'jett.ionic.filter.bar', 'uiGmapgoogle-maps'])
 
 .run(function ($ionicPlatform, AppZanatlijaFactory, $localStorage, $http, $state) {
 
--- www/scripts/controllers.js
+++ www/scripts/controllers.js
@@ -193,33 +193,63 @@ angular.module('starter')
 	$ionicSideMenuDelegate.toggleLeft();
 })
 
-.controller("MapaSingleController", function ($scope, $ionicSideMenuDelegate) {
-	function initMap() {
+.controller("MapaSingleController", function ($scope, $ionicSideMenuDelegate, $ionicLoading, $compile, $window, $stateParams) {
+	function initialize() {
 		var map = new google.maps.Map(document.getElementById('map'), {
-		    zoom: 8,
-		    center: {lat: -34.397, lng: 150.644}
+			zoom: 17,
+			center: {lat: -34.397, lng: 150.644}
 		});
 		var geocoder = new google.maps.Geocoder();
-
-		document.getElementById('submit').addEventListener('click', function() {
-    	geocodeAddress(geocoder, map);
+		geocodeAddress(geocoder, map);
+		$scope.map = map;
+	}
+	function geocodeAddress(geocoder, resultsMap) {
+	  var address = $stateParams.address;
+	  geocoder.geocode({'address': address}, function(results, status) {
+	    if (status === google.maps.GeocoderStatus.OK) {
+	      resultsMap.setCenter(results[0].geometry.location);
+	      var marker = new google.maps.Marker({
+	        map: resultsMap,
+	        position: results[0].geometry.location
+	      });
+	    } else {
+	      alert('Geocode was not successful for the following reason: ' + status);
+	    }
 	  });
 	}
 
-	function geocodeAddress(geocoder, resultsMap) {
-		var address = document.getElementById('address').value;
-		geocoder.geocode({'address': address}, function(results, status) {
-		    if (status === google.maps.GeocoderStatus.OK) {
-				resultsMap.setCenter(results[0].geometry.location);
-				var marker = new google.maps.Marker({
-					map: resultsMap,
-					position: results[0].geometry.location
-				});
-		    } else {
-				alert('Geocode was not successful for the following reason: ' + status);
-		    }
-		});
-	}
+	    $window.initialize = initialize; // callback in global context
+
+	    function loadScript(src) {
+	        var script = document.createElement("script");
+	        script.type = "text/javascript";
+	        document.getElementsByTagName("head")[0].appendChild(script);
+	        script.src = src;
+	    }
+
+	    loadScript('http://www.google.com.mt/jsapi');
+	    loadScript('http://maps.googleapis.com/maps/api/js?key=&v=3&sensor=true&callback=initialize');
+
+
+
+	    $scope.centerOnMe = function () {
+	        if (!$scope.map) {
+	            return;
+	        }
+
+	        $scope.loading = $ionicLoading.show({
+	            content: 'Getting location',
+	            showBackdrop: false
+	        });
+
+	        navigator.geolocation.getCurrentPosition(function (pos) {
+	            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+	            $scope.loading.hide();
+	        }, function (error) {
+	            alert('Unable to get location: ' + error.message);
+	        });
+	    };
+
 })
 
 .controller("MojiOglasiController", function ($scope, $ionicSideMenuDelegate, AppZanatlijaFactory, $localStorage, $ionicFilterBar) {
