14a90989bd29ca9eff148b83baf789beaab2dd74
jovanakovac <jovanakovac@Jovana.jovana.local>
2016-04-15 10:03:28 +0200

    IT250-DZ07-DZ08-Jovana_Kovac_1593

---
+++ IT250-DZ08-Jovana-Kovac_1593/src/main/java/com/mycompany/methotels/interfaces/RezervacijeDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.methotels.interfaces;
+
+import com.mycompany.methotels.entities.Rezervacija;
+
+/**
+ *
+ * @author Jovana Kovac
+ */
+public interface RezervacijeDAO {
+
+    public void dodajRezervaciju(Rezervacija rezervacija);
+}
---
+++ IT250-DZ08-Jovana-Kovac_1593/src/main/java/com/mycompany/methotels/pages/DodavanjeSobe.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.methotels.pages;
+
+import com.mycompany.methotels.entities.Hoteli;
+import com.mycompany.methotels.interfaces.HotelsDAO;
+import com.mycompany.methotels.entities.Sobe;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+/**
+ *
+ * @author Jovana Kovac
+ */
+public class DodavanjeSobe {
+
+    @Property
+    private Sobe sobe;
+    @Property
+    private Hoteli hoteli;
+    @Property
+    private Sobe onesoba;
+    @Inject
+    private HotelsDAO hotelsDao;
+    @Property
+    private List<Sobe> sveSobe;
+    @Property
+    @Persist
+    private List<Hoteli> sviHoteli;
+
+    void onActivate() {
+        if (sveSobe == null) {
+            sveSobe = new ArrayList<Sobe>();
+        }
+        sveSobe = hotelsDao.getListaSvihSoba();
+    }
+
+    public String getIme() {
+        return hoteli.getIme();
+    }
+
+    @CommitAfter
+    Object onSuccess() {
+        hotelsDao.dodajSobu(sobe);
+        return this;
+    }
+}
